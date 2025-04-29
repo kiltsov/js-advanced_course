07b57a65672999030cd10dd3a1fcce3ac06409c8
Kiltsov Anton <kiltsov.anton@gmail.com>
2025-04-29 12:08:27 +0300

    5.4. Упражнение - Проверка на день рождения

---
+++ date.js
@@ -0,0 +1,74 @@
+'use strict';
+
+/*
+
+//
+// 5.1. Создание дат
+//
+
+const now = new Date();
+console.log(now);
+
+console.log(new Date('02-01-2023'));
+console.log(new Date('02/01/2023'));
+
+//
+// 5.2. Операции с датами
+//
+
+const date1 = new Date(2024, 10, 15);
+const date2 = new Date(2024, 11, 15);
+
+console.log(Number(date1));
+console.log(date2 - date1);
+
+function getDaysBetweenDate(dateFirst, dateSecond) {
+	return (date2 - date1) / (1000 * 60 * 60 * 24);
+}
+
+console.log(getDaysBetweenDate(date2, date1));
+
+//
+// 5.3. Сравнение дат
+//
+
+const first = new Date(2024, 10, 4);
+const second = new Date(2024, 10, 4);
+
+console.log(first < second);
+console.log(first == second);
+console.log(first === second);
+
+console.log(first.getTime() == second.getTime());
+console.log(first.getTime() === second.getTime());
+console.log(Number(first) === Number(second));
+
+*/
+
+//
+// 5.4. Упражнение - Проверка на день рождения
+//
+
+const user = {
+	name: 'Vasya',
+	birtday: '04/29/2025'
+};
+
+const user2 = {
+	name: 'Petua',
+	birtday: '12/29/2023'
+};
+
+function isBirthday(userObj) {
+	const currentDate = new Date();
+	const userDate = new Date(userObj.birtday);
+
+	// console.log(currentDate.getDate());
+	// console.log(userDate.getDate());
+
+	return currentDate.getMonth() === userDate.getMonth() ? 
+	currentDate.getDay() === userDate.getDay() ? true : false : false;
+};
+
+console.log(isBirthday(user));
+console.log(isBirthday(user2));
